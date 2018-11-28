f3ce905d76a76770a59350356d1791893f7d432d
Aretha Walls <admin@arethawalls.com>
2018-11-28 13:44:24 -0700

    nextTime appears to be working

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,24 @@
 $(document).ready(function () {
-    var allTrains = (localStorage.getItem('allTrains') || []);
-    var trainCount = (localStorage.getItem('trainCount') || 0);
+    let allTrains = (localStorage.getItem('allTrains') || []);
+    let trainCount = (localStorage.getItem('trainCount') || 0);
 
+    function nextTime(firstTime, frequency) {
+        const now = moment();
+        let next = moment(`${firstTime} ${now.date()} ${now.month() + 1} ${now.year()}`, 'kk:mm DD MM YYYY', true);
+        while(next.isBefore(now, 'minute')) {
+            next.add(frequency, 'm');
+        }
+        return next;
+    }
+   
     function makeRow(name, destination, starts, frequency) {
         return $('<tr>').html(`<td>${name}</td><td>${destination}</td><td>${frequency}<td>`);
     }
 
     // $('#submit').on('click', function(e) {
     $('form').submit( function(e) { 
-       e.preventDefault();
+        e.preventDefault();
+        nextTime($('#first-time').val().trim(), $('#frequency').val().trim());
         $('table').append(
             makeRow($('#train-name').val(), $('#destination').val(), 
             $('#first-time').val(), $('#frequency').val())
